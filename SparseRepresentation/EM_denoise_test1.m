af0efd31b32e0ebffdac1b0c8b0fd87647ba8a68
thanujadax <thanujadax@gmail.com>
2012-07-20 11:29:05 +0200

    denoise test1 file added

--- SparseRepresentation/EM_denoise_test1.m
+++ SparseRepresentation/EM_denoise_test1.m
@@ -3,10 +3,36 @@
 
 clear
 %% Parameters
-bb = 8; % block size
+bb = 24; % block size
 RR = 4; % redundancy factor
-K = RR*bb^2; % number of atoms in the dictionary
+%K = RR*bb^2; % number of atoms in the dictionary
+K = 512;
 
-sigma = 25;
-pathForImages = '/home/thanuja/matlabprojects/data/mitoData';
-imageName = '';
+sigma = 50;
+pathForImages = '/home/thanuja/matlabprojects/data/mitoData/';
+imageName = 'stem1.png';
+
+%%
+[IMin,pp]=imread(strcat([pathForImages,imageName]));
+% fixing input image format
+IMin=im2double(IMin);
+if (length(size(IMin))>2)
+    IMin = rgb2gray(IMin);
+end
+if (max(IMin(:))<2)
+    IMin = IMin*255;
+end
+%% Denoising using K-SVD
+% dictionary is trained on the noisy image
+[IoutAdaptive,output] = denoiseImageKSVD(IMin, sigma,K,bb);
+
+%PSNROut = 20*log10(255/sqrt(mean((IoutAdaptive(:)-IMin0(:)).^2)));
+figure();
+imshow(IMin,[]); 
+title('Noisy image');
+figure();
+imshow(IoutAdaptive,[]); 
+title('Clean Image by Adaptive dictionary');
+figure;
+I = displayDictionaryElementsAsImage(output.D, floor(sqrt(K)), floor(size(output.D,2)/floor(sqrt(K))),bb,bb);
+title('The dictionary trained on patches from the noisy image');
