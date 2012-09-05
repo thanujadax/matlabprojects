ca2694737c73cc99005362c2cc571005887b9c28
thanujadax <thanujadax@gmail.com>
2012-09-05 11:45:45 +0200

    added two files in SparseRepresentations: reusing a denoised dictionary

---
+++ SparseRepresentation/denoiseDictReuse.m
@@ -0,0 +1,17 @@
+% Thanuja 05.09.2012
+% Algorithm that denoises unseen images using a denoised dictionary, for EM
+% images
+% uses the KSVD library
+
+%% Learn the dictionary Dn from a noisy image In
+
+
+%% Learn the denoised dictionary D0 corresponding to the noisy image In
+
+
+%% For an unseen image Iu, learn the sparse codes alpha using 
+% the noisy dictionary Dn
+
+
+%% Reconstruct the unseen image Iu using the sparse codes alpha learned above 
+% and the denoised dictionary D0 
