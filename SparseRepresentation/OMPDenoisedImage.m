838a184791c157c80085b162ef05c2399651305a
thanujadax <thanujadax@gmail.com>
2012-09-14 19:20:50 +0200

    working on modularising dictionary learning and denoising.

---
+++ SparseRepresentation/OMPDenoisedImage.m
@@ -0,0 +1,51 @@
+function IOut = OMPDenoisedImage(sigma,C)
+
+errT = sigma*C;
+
+%blocks = im2col(Image,[NN1,NN2],[bb,bb],'sliding');
+while (prod(floor((size(Image)-bb)/slidingDis)+1)>maxBlocksToConsider)
+    slidingDis = slidingDis+1;
+end
+[blocks,idx] = my_im2col(Image,[bb,bb],slidingDis);
+
+if (waitBarOn)
+    newCounterForWaitBar = (param.numIteration+1)*size(blocks,2);
+end
+
+
+% go with jumps of 30000
+for jj = 1:30000:size(blocks,2)
+    if (waitBarOn)
+        waitbar(((param.numIteration*size(blocks,2))+jj)/newCounterForWaitBar);
+    end
+    jumpSize = min(jj+30000-1,size(blocks,2));
+    if (reduceDC)
+        vecOfMeans = mean(blocks(:,jj:jumpSize));
+        blocks(:,jj:jumpSize) = blocks(:,jj:jumpSize) - repmat(vecOfMeans,size(blocks,1),1);
+    end
+    
+    % Coefs = mexOMPerrIterative(blocks(:,jj:jumpSize),Dictionary,errT);
+    Coefs = OMPerr(Dictionary,blocks(:,jj:jumpSize),errT);
+    if (reduceDC)
+        blocks(:,jj:jumpSize)= Dictionary*Coefs + ones(size(blocks,1),1) * vecOfMeans;
+    else
+        blocks(:,jj:jumpSize)= Dictionary*Coefs ;
+    end
+end
+
+count = 1;
+Weight = zeros(NN1,NN2);
+IMout = zeros(NN1,NN2);
+[rows,cols] = ind2sub(size(Image)-bb+1,idx);
+for i  = 1:length(cols)
+    col = cols(i); row = rows(i);        
+    block = reshape(blocks(:,count),[bb,bb]);
+    IMout(row:row+bb-1,col:col+bb-1)=IMout(row:row+bb-1,col:col+bb-1)+block;
+    Weight(row:row+bb-1,col:col+bb-1)=Weight(row:row+bb-1,col:col+bb-1)+ones(bb);
+    count = count+1;
+end;
+
+if (waitBarOn)
+    close(h);
+end
+IOut = (Image+0.034*sigma*IMout)./(1+0.034*sigma*Weight);
