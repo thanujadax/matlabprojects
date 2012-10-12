49329293b61fa43f0bd477467866f9cee7995f6b
thanujadax <thanujadax@gmail.com>
2012-10-12 18:43:28 +0200

    mrf proj2

---
+++ MRF_dict/main.m
@@ -0,0 +1,21 @@
+% MRF learning
+
+% parameters and variables
+hasInitDict = 1;
+pathToDict = '';
+
+% get the dictionary words
+% build the association matrices 
+%   top-down
+%   left-right
+% (remember to add 1)
+
+% Build MRF
+% define potentials
+%   1-clique:   Vc(f_i) = (pixel error)
+%   pairwise:   Vc(f_i,f_i') = (from associations matrix)
+
+% run energy minimization
+
+% sample MRF to generate sample image
+
