dccb0621fa2df32c65e25f0b346ca639b24a3d9a
thanujadax <thanujadax@gmail.com>
2012-10-24 10:57:20 +0200

    corrected getHorizonatalAssociations. First implementation of getVertical associations

--- MRF_dict/getHorizontalAssociations.m
+++ MRF_dict/getHorizontalAssociations.m
@@ -1,46 +1,22 @@
-function AMat = getHorizontalAssociations(coefmat)
+function AMat = getHorizontalAssociations(coefmat,outfilename)
 
 % coefmat - sparse matrix of coefficients. Rows correspond to the
 % dictionary words and the columns correspond to the overlapping patches of
 % the image.
 
-% % fixed parameters
-% numwords = 1;           % number of active coefficients per patch
-% HorizontalAssociations = ones(size(Dictionary));
-% 
-% 
-% [NN1,NN2] = size(image);
-% 
-% while (prod(floor((size(Image)-bb)/slidingDis)+1)>maxBlocksToConsider)
-%     slidingDis = slidingDis+1;
-% end
-% [blocks,idx] = my_im2col(image,[bb,bb],slidingDis);
-% 
-% % processing 30000 blocks at a time
-% for jj = 1:30000:size(blocks,2)
-%     jumpSize = min(jj+30000-1);
-%     
-%     if(reduceDC)
-%         vecOfMeans = mean(blocks(:,jj:jumpSize));
-%         blocks(:,jj:jumpSize) = blocks(:,jj:jumpSize) - repmat(vecOfMeans,size(blocks,1),1);
-%     end
-%     
-%     Coefs = OMP([FixedDictionaryElement,Dictionary],blocks, numwords);
-%     if(reduceDC)
-%         blocks(:,jj:jumpSize)= Dictionary*Coefs + ones(size(blocks,1),1) * vecOfMeans;
-%     else
-%         blocks(:,jj:jumpSize)= Dictionary*Coefs ;
-%     end
-%     
-% end
-
 AMat = ones(size(coefmat,1));     % initializing association matrix
 
 ind1 = find(coefmat(:,1));       % index of the nonzero element of column 1 (index of atom for patch #1)
-for i = 2:size(coefmat,2)
+% if size(ind1,1) == 0
+%     ind1 = 0;
+% end
+    
+for i = 2:(size(coefmat,2)-1)
     ind2 = find(coefmat(:,i));  % index of the word for the next patch
-    AMat(ind1,ind2) = AMat(ind1,ind2) + 1;
-    ind = ind2;
+    if (size(ind2,1)>0 && size(ind1,1)>0)
+        AMat(ind1,ind2) = AMat(ind1,ind2) + 1;
+    end
+    ind1 = ind2;
 end
 
 
---
+++ MRF_dict/getVerticalAssociations.m
@@ -0,0 +1,18 @@
+function AMat = getVerticalAssociations(coefmat,rowSize,outfilename)
+
+% coefmat - sparse matrix of coefficients. Rows correspond to the
+% dictionary words and the columns correspond to the overlapping patches of
+% the image.
+% rowSize - number of patches in one row of the image
+
+
+AMat = ones(size(coefmat,1));     % initializing association matrix
+    
+for i = 1:(size(coefmat,2)-rowSize)
+    ind1 = find(coefmat(:,i));
+    j = i + rowSize;            % downward neighbor of patch i
+    ind2 = find(coefmat(:,j));  % index of the word for the next patch
+    if (size(ind2,1)>0 && size(ind1,1)>0)
+        AMat(ind1,ind2) = AMat(ind1,ind2) + 1;
+    end
+end
