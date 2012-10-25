7176af6f80e0579141b9910e64a473e4c46b8b68
thanujadax <thanujadax@gmail.com>
2012-10-25 21:51:35 +0200

    finished implementing the main part for sampling. TODOs in the code!

--- MRF_dict/sampleFromMRF_simple.m
+++ MRF_dict/sampleFromMRF_simple.m
@@ -16,7 +16,7 @@
 
 function IOut = sampleFromMRF_simple(imgDim,bb,Hmat,Vmat,prior,Dictionary) 
 
-% Initialize random field
+                                        % Initialize random field
 rows = imgDim(1) - bb + 1;              % number of overlapping image patches per column
 cols = imgDim(2) - bb + 1;              % number of overlapping image patches per row
 
@@ -25,10 +25,49 @@ totPatches = rows*cols;                 % total number of patches to be generate
 coefMat = sparse(size(Dictionary,2),totPatches);
 
 % Pick a random word for the first image patch
-wordInd = ceil(rand(1) * size(Dictionary,2)) ;
+wordInd = ceil(rand(1) * size(Dictionary,2)) ; % use prior ? TODO
 coefMat(wordInd,1) = 1;                 % assign the word to the first patch
 for i = 2:totPatches
-    % loop until all the patches are processed
+                                        % loop until all the patches are processed
+    if(mod(i,cols) ~= 1) % i is not in the first colum
+        wordInFirstCol = 0;             % flag
+                                        % sample a word from Hmat given i-1 th word
+        [wordIndH,probH] = sampleWordToRight(wordToLeft,Hmat);       % TODO
+    else
+        wordInFirstCol = 1;
+    end
+    if(i>cols)                          % i is not in the first row
+        wordInFirstRow = 0;
+                                        % sample a word from Vmat given the one above
+        [wordIndV,probV] = sampleWordToBottom(wordToTop,Vmat);      % TODO
+    else
+        wordInFirstRow = 1;
+    end
+        
+    % assigning a word to the patch i based on the above information
+    if(wordInFirstCol)
+        % we only have to consider the vertical association
+        wordInd = wordIndV;
+    elseif (wordInFirstRow)
+        % we only have to consider the horizontal associations
+        wordInd = wordIndH;
+    else
+        % we have to consider both associations
+        % pick the stronger candidate based on the probability
+        if (probH > probV)
+            wordInd = wordIndH;
+        elseif (probH == probV)
+            if(rand(1)<0.5)
+                wordInd = wordIndH;
+            else
+                wordInd = wordIndV;
+            end
+        else
+            wordInd = wordIndV;
+        end
+        
+    end
     
+    coefMat(wordInd,i) = 1;             % assigning the word to the patch i
     
 end
