ca3b8085ec8bf47805889b0bb66456caf017bedc
thanujadax <thanujadax@gmail.com>
2012-10-25 11:08:01 +0200

    small update to return coefficients from least_squares_match

--- SparseRepresentation/least_squares_match.m
+++ SparseRepresentation/least_squares_match.m
@@ -6,13 +6,18 @@ function Coefs = least_squares_match(Data, Dictionary)
         
         bestWord = -1;
         minDistance = 0;
+        bestScale = 0;
         
         dataL2norm = Data(:,i)'*Data(:,i);
         
+        % find the best dictionary word regarding the L2 norm without
+        % consideration of the scale
         for j = 1:size(Dictionary, 2)
             
             dataWordDotProduct = Data(:,i)'*Dictionary(:,j);
             
+            % scaling factor for the data-point to minimize the L2 norm to
+            % the current dictionary word
             scale = dataWordDotProduct/dataL2norm;
             
             difference = scale*Data(:,i) - Dictionary(:,j);
@@ -22,8 +27,10 @@ function Coefs = least_squares_match(Data, Dictionary)
                 
                 minDistance = distance;
                 bestWord = j;
+                bestScale = scale;
             end
         end
         
-        Coefs(bestWord, i) = 1.0;
+        % the coefficient is the inverse scale of the data-point
+        Coefs(bestWord, i) = 1/bestScale;
     end        
