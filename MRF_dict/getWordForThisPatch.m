5c8ddabc33346a26f65f57f9ecffcfb301964f7a
thanujadax <thanujadax@gmail.com>
2012-10-29 16:42:10 +0100

    bug from z calculation fixed. getZ not required.

--- MRF_dict/getWordForThisPatch.m
+++ MRF_dict/getWordForThisPatch.m
@@ -4,7 +4,7 @@ function wordInd = getWordForThisPatch(patchID,Dictionary,verticalAMat,horizonta
 % inputData - row vector containing the current patch. or 0 if no input
 % data is provided
 wordInd = 0;    % initialize
-z = getZ(Dictionary,inputData,verticalAMat,horizontalAMat,currentLabels,rowSize,colSize);
+%z = getZ(Dictionary,inputData,verticalAMat,horizontalAMat,currentLabels,rowSize,colSize);
 
 % f2 column vector containing pairwise potentials for this patch
 % each elements assumes this patch is labeled Dictionary(i)
@@ -12,13 +12,16 @@ dictionarySize = size(Dictionary,2);
 f2 = getPairwisePotentials(patchID,currentLabels,dictionarySize,verticalAMat,horizontalAMat,rowSize);
 if(inputData==0)
     % no unary potentials
-    condProbs = exp(- f2) ./ z;
+    %condProbs = exp(- f2) ./ z;
+    condProbs = exp(- f2);
 else
     % with unary potentials
     f1 = getUnaryPotentialVec(inputData,Dictionary,sigma);
-    condProbs = exp(-f1-f2) ./ z;
+    %condProbs = exp(-f1-f2) ./ z;
+    condProbs = exp(-f1-f2);
 end
-
+z = sum(condProbs);
+condProbs = condProbs./z;
 %% sampling from the conditional prob distribution
 cumulativeProb = 0;
 x = rand(1);
