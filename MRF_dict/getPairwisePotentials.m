7e2877dfb0268e594f7015bd7046f357fe067d8b
thanujadax <thanujadax@gmail.com>
2012-11-01 17:58:29 +0100

    bug fixed and incorporation of normalized association matrices into getPairwisePotentials

--- MRF_dict/getPairwisePotentials.m
+++ MRF_dict/getPairwisePotentials.m
@@ -1,29 +1,38 @@
 function f2 = getPairwisePotentials(patchID,currentLabels,dictionarySize,...
                     verticalAMat,horizontalAMat,rowSize)
+                
+% normalize association matrices if they are not already normalized
+verticalAMat_colNormalized = normalizeAMat(verticalAMat,1);
+verticalAMat_rowNormalized = normalizeAMat(verticalAMat,0);
+
+horizontalAMat_colNormalized = normalizedAMat(horizontalAMat,1);
+horizontalAMat_rowNormalized = normalizedAMat(horizontalAMat,0);
+
 % for a single patch considering its neighborhoot
 pairwisePotMat = zeros(dictionarySize,4);
 for i = 1:dictionarySize
 
     if(patchID - rowSize > 0)
-        pairwisePotMat(i,1) = -1.*log(verticalAMat(i,currentLabels(patchID-rowSize)));
+        % pairwisePotMat(i,1) = -1.*log(verticalAMat(i,currentLabels(patchID-rowSize)));
+        pairwisePotMat(i,1) = -1.*log(verticalAMat_colNormalized(i,currentLabels(patchID-rowSize)));
     else
         pairwisePotMat(i,1) = 0;    % nothing on top
     end
     % condCostGivenBottom
     if(patchID+rowSize <= size(currentLabels,2))
-        pairwisePotMat(i,2) = -1.*log(verticalAMat(currentLabels(patchID+rowSize),i));
+        pairwisePotMat(i,2) = -1.*log(verticalAMat_rowNormalized(currentLabels(patchID+rowSize),i));
     else
         pairwisePotMat(i,2) = 0;    % nothing to the bottom
     end
     % condCostGivenLeft
     if(patchID-1 > 0 && mod(patchID,rowSize)>1)
-        pairwisePotMat(i,3) = -1.*log(horizontalAMat(i,currentLabels(patchID-1)));
+        pairwisePotMat(i,3) = -1.*log(horizontalAMat_colNormalized(i,currentLabels(patchID-1)));
     else
         pairwisePotMat(i,3) = 0;    % nothing to the left
     end
     % condCostGivenRight
     if(patchID+1<=size(currentLabels,2) && mod(patchID,rowSize)~=0)
-        pairwisePotMat(i,4) = -1.*log(horizontalAMat(currentLabels(patchID+1),i));
+        pairwisePotMat(i,4) = -1.*log(horizontalAMat_rowNormalized(currentLabels(patchID+1),i));
     else
         pairwisePotMat(i,4) = 0;    % nothing to the right
     end
--- MRF_dict/normalizeAMat.m
+++ MRF_dict/normalizeAMat.m
@@ -6,17 +6,19 @@ function normalizedMat = normalizeAMat(inMat,normalizeColumns)
 % normalizeColumns: if set, normalization is done columnwise. if zero,
 % normalization is done per each row.
 
+normalizedMat = zeros(size(inMat));
+
 if(normalizeColumns)
     colSum = sum(inMat,1);      % gets a row vector containing the columnwise sums
 
     for i = 1 : size(inMat,2)
-        normalizedMat = inMat(:,i)./colSum(i);      % column i normalized
+        normalizedMat(:,i) = inMat(:,i)./colSum(i);      % column i normalized
     end
 else
         rowSum = sum(inMat,2);      % gets a col vector containing the columnwise sums
 
     for i = 1 : size(inMat,1)
-        normalizedMat = inMat(i,:)./rowSum(i);      % row i normalized
+        normalizedMat(i,:) = inMat(i,:)./rowSum(i);      % row i normalized
     end
 
 end
