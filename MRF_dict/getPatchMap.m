1d0cd3a28e4471e67fd07c726e93fcc287021adb
thanujadax <thanujadax@gmail.com>
2012-11-12 14:13:13 +0100

    corrected the transpose effect

--- MRF_dict/getPatchMap.m
+++ MRF_dict/getPatchMap.m
@@ -35,12 +35,12 @@ end
 
 % fill the patch map image with the words of the dictionary
 patchIndex = 1;
-for i = 1:numPatchPerRow
+for i = 1:numPatchPerCol
     startingPoint_i = (i-1)*bb + 1;
-    for j = 1:numPatchPerCol
+    for j = 1:numPatchPerRow
         startingPoint_j = (j-1)*bb + 1;
-        patchMapImage(startingPoint_i:(startingPoint_i+bb-1),...
-            startingPoint_j:(startingPoint_j+bb-1)) = ...
+        patchMapImage(startingPoint_j:(startingPoint_j+bb-1),...
+            startingPoint_i:(startingPoint_i+bb-1)) = ...
                 reshape(Dictionary(:,labelVector(patchIndex)),bb,bb);
         patchIndex = patchIndex + 1;
         
