9e781491e1a6f7b8b90dfb8f6d7f9cdb13e95ae0
thanujadax <thanujadax@gmail.com>
2013-06-20 18:01:35 +0200

    contoursILP: started implementation to obtain face adjacency

---
+++ contourdetection/getFaceAdjFromJnAdjGraph.m
@@ -0,0 +1,8 @@
+function faceAdj = getFaceAdjFromJnAdjGraph(A)
+% input: adjacency graph of junctions (planar graph)
+% output: adjacency graph of the faces of the input planar graph
+
+% draw input graph in XY plane
+
+% traverse the graph to pick one cell at a time
+
