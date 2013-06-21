126f644c22368b801ed58b58c4d2e341e5a69773
thanujadax <thanujadax@gmail.com>
2013-06-21 14:47:32 +0200

    common: added method to add thick border around an image

---
+++ common/addThickBorder.m
@@ -0,0 +1,18 @@
+function outImg = addThickBorder(inImg,margin,val)
+% adds a thick border of pixel value val of size 'margin' to the image
+
+[sizeR,sizeC] = size(inImg);
+
+newR = sizeR + margin * 2;
+newC = sizeC + margin * 2;
+
+outImg = ones(newR,newC).*val;
+
+% set the input image inside the outImg
+startRow = margin + 1;
+endRow = newR - margin;
+startCol = margin + 1;
+endCol = newC - margin;
+
+outImg(startRow:endRow,startCol,endCol) = inImg;
+
