6ddec4bb1bb5badb41234ac907ccc92846f73d48
thanujadax <thanujadax@gmail.com>
2013-06-28 15:12:01 +0200

    contours-faceAdj: intersection of the normal bisectors - done

--- contourdetection/getPerpendicularBisectorIntersection.m
+++ contourdetection/getPerpendicularBisectorIntersection.m
@@ -1,18 +1,28 @@
-function [x,y] = getPerpendicularBisectorIntersection(edge1_pixels,edge2_pixels,sizeR,sizeC,...
-                    edges2nodes,nodeInds)
+function [x,y] = getPerpendicularBisectorIntersection(edge1_pixels,edge2_pixels,...
+    edge1_id,edge2_id, sizeR,sizeC,edges2nodes,nodeInds)
+
+x = [];
+y = [];
 
 % get the midpoint
 midpoint1 = getMidpoint(edge1_pixels);
 midpoint2 = getMidpoint(edge2_pixels);
-
+% convert to xy coordinates
+[midxy1.y,midxy1.x] = ind2sub([sizeR sizeC],midpoint1);
+[midxy2.y,midxy2.x] = ind2sub([sizeR sizeC],midpoint2);
 
 % get the tangent at the midpoint
-
-
+% (eqn of the lines)
+tangent1 = getTangentAtPoint(edge1_pixels,midpoint1,edges2nodes,edgeIndsAll,...
+    sizeR,sizeC,edge1_id,nodeInds);
+tangent2 = getTangentAtPoint(edge2_pixels,midpoint2,edges2nodes,edgeIndsAll,...
+    sizeR,sizeC,edge2_id,nodeInds);
 
 % draw perpendiculars to the tangents at the midpoints
+normal1 = getNormalToLine(line1,midxy1);
+normal2 = getNormalToLine(line2,midxy2);
 % get the intersection
-
+[x,y] = getIntersectionOfTwoLines(normal1,normal2);
 end
 
 function midpoint = getMidpoint(edgepixels)
@@ -23,28 +33,69 @@ midpoint = edgepixels(midpoint_pos);
 
 end
 
-function [mdegrees,c] = getTangent(edgepixels,midpoint,edges2nodes,edgeInds)
-    numEdgePix = numel(edgepixels)
+function lineEqn = getTangentAtPoint(edgepixels,midpoint,edges2nodes,edgeIndsAll,...
+    sizeR,sizeC,edgeID,nodeInds)
+
+    midpos = find(edgepixels==midpoint);
+    numEdgePix = numel(edgepixels);
     if(numEdgePix>2)
-        midpos = find(edgepixels==midpoint);
         pt1 = edgepixels(midpos-1);
-        pt2 = edgepixels(midpos+1);    
+        pt2 = edgepixels(midpos+1);        
     else
         % there're only 2 or 1 pixels for this edge. then we need the nodepixels
         % on either side
-        pt1 = edges2nodes()
+        edgeNodes_listInds = edges2nodes(edgeIndsAll==edgeID);
+        pt1 = nodeInds(edgeNodes_listInds(1));
+        pt2 = nodeInds(edgeNodes_listInds(2));        
     end
-    [x1,y1] = ind2sub([sizeR,sizeC],pt1);
-    [x2,y2] = ind2sub([sizeR,sizeC],pt2);
-    [mdegrees,c] = getStraightLineEqn(x1,y1,x2,y2); 
+    pt3 = edgespixels(midpos);
+    
+    % create structure array to contain the 3 points
+    p = struct([]);
+    [p(1).y, p(1).x] = ind2sub([sizeR,sizeC],pt1);
+    [p(2).y, p(2).x] = ind2sub([sizeR,sizeC],pt2);
+    [p(3).y, p(3).x] = ind2sub([sizeR,sizeC],pt3);
+    
+    lineEqn = getStraightLineEqn(p); 
     
 end
 
-function [mdegrees,c] = getStraightLineEqn(x1,y1,x2,y2)
-    mdegrees = atan2d((y2-y1),(x2-x1));
-    if(mdegrees==90)
-        c = 0; % x has just one value for all values of y
+function lineEqn = getStraightLineEqn(p)
+    % p contains 3 points. solve ax + by + c = 0 to find a, b, c.
+    syms a b c x y eq1 eq2 eq3 eq
+    eq = a*x + b*y + c;
+    eq1 = subs(eq,[x,y],[p(1).x,p(1).y]);
+    eq2 = subs(eq,[x,y],[p(2).x,p(2).y]);
+    eq3 = subs(eq,[x,y],[p(3).x,p(3).y]);
+    [lineEqn.a,lineEqn.b,lineEqn.c] = solve(eq1,eq2,eq3,a,b,c);
+end
+
+function normalLine = getNormalToLine(lineEqn,midxy)
+    syms a b c a0 b0 c0 x y eq1 eq2
+    eq1 = a*a0 + b*b0;
+    eq2 = a*x + b*y + c;
+    eq3 = subs(eq1,[a0,b0,b],[lineEqn.a,lineEqn.b,1]);
+    eq4 = subs(eq2,[x,y,b],[midxy.x,midxy.y,1]); % subs b = 1.
+    % then a = -gradient, c = -intercept
+    [a,c] = solve(eq3,eq4,a,c);
+    if(a==inf || a==(-inf))
+        eq4 = subs(eq2,[x,y,a,b],[midxy.x,midxy.y,1,0]); % subs a=1,b=0
+        % then the eqn is in the form x = c
+        normalLine.a = 1;
+        normalLine.b = 0;
+        normalLine.c = solve(eq4,c);
     else
-        c = y1 - x1 .* tand(mdegrees);
+        normalLine.a = a;
+        normalLine.b = 1;
+        normalLine.c = c;
     end
+end
+
+function [x,y] = getIntersectionOfTwoLines(line1,line2)
+% normalize lines
+syms a b c x y eq1 eq2 eq
+eq = a*x + b*y + c;
+eq1 = subs(eq,[a,b,c],[line1.a,line1.b,line1.c]);
+eq2 = subs(eq,[a,b,c],[line2.a,line2.b,line2.c]);
+[x,y] = solve(eq1,eq2,x,y);
 end
