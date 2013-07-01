0c77f420d0c8816ca64a158dfedb9a9c78b1cad5
thanujadax <thanujadax@gmail.com>
2013-07-02 00:00:52 +0200

    contours: getCentroid reimplemented according to wiki explanation of the calculation of centroid of polygon using vertices

--- contourdetection/getCellCentroid.m
+++ contourdetection/getCellCentroid.m
@@ -12,24 +12,60 @@ function cellCentroid = getCellCentroid(edgeIDset_cell,edges2pixels,edgeIDs_all,
 %   cellCentroid = [x; y] coordinates of the approximate centroid of the
 %   cell
 
-% get all the edges bounding the cell
+% % get all the edges bounding the cell
+% numEdgesForCell = numel(edgeIDset_cell);
+% edgeListInds = zeros(numEdgesForCell,1);
+% X = [];
+% Y = [];
+% for i=1:numEdgesForCell
+%     edgeListInds(i) = find(edgeIDs_all==edgeIDset_cell(i));
+%     clear edge_i_pixels
+%     edge_i_pixels = edges2pixels(edgeListInds(i),:);
+%     edge_i_pixels(1) = []; % first element is the edgeID
+%     edge_i_pixels = edge_i_pixels(edge_i_pixels>0);
+%     clear x y
+%     [y x] = ind2sub([sizeR sizeC],edge_i_pixels');
+%     X = [X; x];
+%     Y = [Y; y];
+% end
+% 
+% meanx = floor(mean(X));
+% meany = floor(mean(Y));
+% 
+% cellCentroid = [meanx meany];
+
+%% method 2
+% get the vertices (x,y)
 numEdgesForCell = numel(edgeIDset_cell);
 edgeListInds = zeros(numEdgesForCell,1);
-X = [];
-Y = [];
+nodeset = [];
 for i=1:numEdgesForCell
     edgeListInds(i) = find(edgeIDs_all==edgeIDset_cell(i));
-    clear edge_i_pixels
-    edge_i_pixels = edges2pixels(edgeListInds(i),:);
-    edge_i_pixels(1) = []; % first element is the edgeID
-    edge_i_pixels = edge_i_pixels(edge_i_pixels>0);
-    clear x y
-    [y x] = ind2sub([sizeR sizeC],edge_i_pixels');
-    X = [X; x];
-    Y = [Y; y];
+    nodes = edges2nodes(edgeListInds(i),:);
+    nodeset = [nodeset nodes];
 end
+nodeset = unique(nodeset);
+nodeset_pixinds = nodeInds(nodeset);
+[y,x] = ind2sub([sizeR sizeC],nodeset_pixinds');
+N=numel(x);
+y(end+1) = y(1);
+x(end+1) = x(1);
+% calculate area A
+sum = 0;
+for i=1:N
+    sum = sum + x(i)*y(i+1) - x(i+1)*y(i);
+end
+A = sum/2;
+% cog
+sum = 0;
+for i=1:N
+    sum = sum + (x(i) + x(i+1)) * (x(i)*y(i+1) - x(i+1)*y(i));
+end
+X = sum/(6*A);
+sum = 0;
+for i=1:N
+    sum = sum + (y(i) + y(i+1)) * (x(i)*y(i+1) - x(i+1)*y(i));
+end
+Y = sum/(6*A);
 
-meanx = floor(mean(X));
-meany = floor(mean(Y));
-
-cellCentroid = [meanx meany];
+cellCentroid = [floor(X) floor(Y)];
