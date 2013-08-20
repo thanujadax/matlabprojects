c070cbdebd6785f54b08a786727687d49d457e0d
thanujadax <thanujadax@gmail.com>
2013-08-20 18:20:06 +0200

    improved RF learning for cell interior classification

--- contourdetection/trainRandomForest_pixelProb.m
+++ contourdetection/trainRandomForest_pixelProb.m
@@ -8,52 +8,64 @@ function forest_pixelProb = trainRandomForest_pixelProb()
 % (-)
 
 %% parameters
-pathForImages = ''; % use wild cards to allow for indices
-pathForLabels = '';
+% use wild cards to allow for indices
+pathForImages_training = '/home/thanuja/Dropbox/data/RF_training_mem/*_trainingImage.tif'; 
+pathForImages_testing = '/home/thanuja/Dropbox/data/RF_training_mem/*_testingImg.tif';
+pathForLabels_training = '/home/thanuja/Dropbox/data/RF_training_mem/*_trainingLabels.tif';
+pathForLabels_testing = '/home/thanuja/Dropbox/data/RF_training_mem/*_testingLabels.tif';
 maxNumberOfSamplesPerClass = 5000;
-LEN_IMG_IND = 4;
-% RF training params
+LEN_IMG_IND = 3;
+% RF training param
 NUM_TREES = 500;
 MTRY = 5;
+% RF testing param
 
-%% read images
-imgFiles = dir(pathForImages);
-labelFiles = dir(pathForLabels);
+% param for membrane feature learning
+  cs = 29;
+  ms = 3;
+  csHist = cs;
+
+%% read training images
+imgFiles_training = dir(pathForImages_training); % images for both training and testing
 
 %% extract features. save separate feature matrices for each training image
-% name of feature matrix: <imageName(1:6)>_fm.mat
-for i=1:length(imgFiles)
-    name = imgFiles(i).name;
+% name of feature matrix: <imageName(1:ind)>_fm.mat
+for i=1:length(imgFiles_training)
+    name = imgFiles_training(i).name;
     % extract features only if feature matrix is not already presaved
-    if ~exist(strcat(name(1:LEN_IMG_IND),'_fm.mat'));
-        disp('extracting membrane features');
-        im = norm01((imresize(imread(imgNames(i).name),1)));
-        fm = getFeatures(im);
+    if ~exist(strcat(name(1:LEN_IMG_IND),'_fm.mat'),'file');
+        disp('extracting features for the training images ...');
+        im = norm01((imresize(imread(name),1)));
+        % fm = getFeatures(im); % TODO
+        fm  = membraneFeatures(im, cs, ms, csHist);
         save(strcat(name(1:LEN_IMG_IND),'_fm.mat'),'fm');
         clear fm
         clear im       
+        disp('Feature extraction of training images done!')
+        disp('Feature matrices saved as *_fm.mat files');
+    else
+        disp('Found precalculated feature matrix for training image %d',i)
     end
-     
 end
 
 %% read training labels
-trainingImgNames = dir(pathForLabels);
+trainingLabelImgNames = dir(pathForLabels_training);
 fmPos = [];
 fmNeg = [];
 
-for i=1:length(imgNames)
-  name = imgNames(i).name;
+for i=1:length(trainingLabelImgNames)
+  name = trainingLabelImgNames(i).name;
   im = imread(name);
   posPos = find(im(:,:,2)==255 & im(:,:,1)==0);
   posNeg = find(im(:,:,1)==255 & im(:,:,2)==0);
   
-  if length(posPos)>0 | length(posNeg)>0
-    load(strcat(name(1:LEN_IMG_IND),'_fm.mat'));
-    fm = reshape(fm,size(fm,1)*size(fm,2),size(fm,3));
-    fm(isnan(fm))=0;
-    fmPos = [fmPos; fm(posPos,:)];
-    fmNeg = [fmNeg; fm(posNeg,:)];
-    clear fm;
+  if ~isempty(posPos) || ~isempty(posNeg)
+      load(strcat(name(1:LEN_IMG_IND),'_fm.mat'));
+      fm = reshape(fm,size(fm,1)*size(fm,2),size(fm,3));
+      fm(isnan(fm))=0;
+      fmPos = [fmPos; fm(posPos,:)];
+      fmNeg = [fmNeg; fm(posNeg,:)];
+      clear fm;
   end
 end
 
@@ -74,3 +86,59 @@ x = double([fmNeg;fmPos]);
 extra_options.sampsize = [maxNumberOfSamplesPerClass, maxNumberOfSamplesPerClass];
 forest_pixelProb = classRF_train(x, y, NUM_TREES,MTRY,extra_options);
 
+%% Evaluation of the learned forest
+% Read test images
+imgFiles_testing = dir(pathForImages_testing);
+% calculate feature matrices for test images
+for i=1:length(imgFiles_testing)
+    name = imgFiles_testing(i).name;
+    % extract features only if feature matrix is not already presaved
+    if ~exist(strcat(name(1:LEN_IMG_IND),'_fm.mat'),'file');
+        disp('extracting features for the test images ...');
+        im = norm01((imresize(imread(name),1)));
+        % fm = getFeatures(im); % TODO
+        fm  = membraneFeatures(im, cs, ms, csHist);
+        save(strcat(name(1:LEN_IMG_IND),'_fm.mat'),'fm');
+        clear fm
+        clear im       
+        disp('Feature extraction of testing images done!')
+        disp('Feature matrices saved as *_fm.mat files');
+    end
+end
+% Read labels for test images
+testingLabelImgNames = dir(pathForLabels_testing);
+fmPos = [];
+fmNeg = [];
+
+for i=1:length(testingLabelImgNames)
+  name = testingLabelImgNames(i).name;
+  im = imread(name);
+  load(strcat(name(1:LEN_IMG_IND),'_fm.mat'));
+  fm = reshape(fm,size(fm,1)*size(fm,2),size(fm,3));
+  fm(isnan(fm))=0;
+  clear fmNeg
+  clear fmPos
+  im=uint8Img(im(:,:,1));
+  imsize = size(im);
+  clear y
+  clear im
+  
+  votes = zeros(imsize(1)*imsize(2),1);
+  
+  disp('prediction for test image %d',i)
+  [y_h,v] = classRF_predict(double(fm), forest);
+  disp('prediction for test image %d done!',i)
+  votes = v(:,2);
+  votes = reshape(votes,imsize);
+  votes = double(votes)/max(votes(:));
+  
+  disp('visualization')
+  tic;
+  im = imread(name);			% 
+  % this illustration uses the thickened skeleton of the segmentation
+  % this is the skeletonized view
+  figure; imshow(makeColorOverlay(votes,im));
+  imwrite(makeColorOverlay(votes,im),strcat(name(1:LEN_IMG_IND),'_overlay.tif'),'tif');
+  % TODO: calculate pixel error
+  
+end
