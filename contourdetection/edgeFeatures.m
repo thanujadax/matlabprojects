fc157badd39133129f9f171db2ee011764813a43
thanujadax <thanujadax@gmail.com>
2013-08-23 19:49:05 +0200

    first implementation of edgeFeature extraction

---
+++ contourdetection/edgeFeatures.m
@@ -0,0 +1,14 @@
+function fm = edgeFeatures(edgepixels,OFR,fm_membrane)
+
+numEdges = size(edgepixels,1);
+numOFRdim = size(OFR,3);
+
+% initialize feature matrix
+fm = zeros(numEdges,numOFRdim);
+for i=1:numEdges
+    % get all edge pixels
+    % get OFR for each pixel (all dimensions)
+    
+    
+end
+
