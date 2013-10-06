1ee6b0d7fa4ac5236fe369080572eaabfaf0f570
thanujadax <thanujadax@gmail.com>
2013-10-06 13:59:15 +0200

    contours: fixed minor bug in ILP5

---
+++ contourdetection/getRegionIDsForAllWsFaces.m
@@ -0,0 +1,15 @@
+function regionIDs = getRegionIDsForAllWsFaces(ws,setOfRegions,edges2pixels)
+% For all the faces in WS, get the corresponding regionID.
+% RegionID is the sequence number of each region in setOfRegions
+
+numWsFaces = max(max(ws));
+
+for i=1:numWsFaces
+    % get internal pixels
+    
+    % get edge pixels
+    
+    % get edges
+    
+    % get regionID
+end
