5e749d762b6c1a2056bc4ae9a882b72ffa34c1ff
thanujadax <thanujadax@gmail.com>
2013-10-08 00:17:44 +0200

    implementing data str for cells

--- contourdetection/getRegionsForCells.m
+++ contourdetection/getRegionsForCells.m
@@ -1,2 +1,21 @@
 function c_cells2regions = getRegionsForCells(faceAdj,offEdgeIDList)
 
+usedRegionList = [];
+numRegions = size(faceAdj,1);
+
+for i=1:numRegions
+    % regionList_i contains the regionIDs that are connected to each other
+    [regionList_i,usedRegionList] = getRegionList(faceAdj,offEdgeIDList); % recursive function
+    
+end
+
+
+
+function [regionList,usedRegionList] = getRegionList(faceAdj,offEdgeIDList)
+numRegions = size(faceAdj,1);
+% for each region
+
+% get all edges connected to it
+% get edges turned off
+% get regions connected via off edges
+
