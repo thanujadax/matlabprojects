b9d314e25dc0fc22ee7e57e587cfdae598a6b37a
thanujadax <thanujadax@gmail.com>
2013-10-27 12:41:05 +0100

    fixed bug - filtered out WSregions which are not maximally inside a cell

--- createTrainingData/getCells2WSregions.m
+++ createTrainingData/getCells2WSregions.m
@@ -4,6 +4,10 @@ function [c_wsIDsInCell,c_internalEdgeIDsInCell,c_extEdgeIDsInCell]...
 % Inputs:
 %   setOfRegions: contains edgeIDs for each wsRegion
 
+
+% Params:
+threshFrac = 0.2; % fraction of WSregion pixels allowed to be OUTSIDE of the cell
+
 c_wsIDsInCell = cell(numLabels,1);
 c_internalEdgeIDsInCell = cell(numLabels,1);
 c_extEdgeIDsInCell = cell(numLabels,1);
@@ -15,12 +19,17 @@ parfor i=1:numLabels
     % label_i corresponds to cell_i
     ws_i = ws;
     setOfRegions_i = setOfRegions;
-    pixelsForLabel_i = (labelImg_indexed==i);
-    wsIDsForLabel_i = ws_i(pixelsForLabel_i);
+    pixelsForLabel_i_logical = (labelImg_indexed==i);
+    wsIDsForLabel_i = ws_i(pixelsForLabel_i_logical);
     wsIDsForLabel_i = unique(wsIDsForLabel_i);
     wsIDsForLabel_i = wsIDsForLabel_i(wsIDsForLabel_i>0);
-    c_wsIDsInCell{i} = wsIDsForLabel_i; 
-    regionIDs = wsIDsForLabel_i - 1;
+    
+    % pick the wsRegions having 4/5 of their pixels in this cell
+    wsIDsForLabel_i_filtered = getGoodWSregions...
+        (wsIDsForLabel_i,ws,pixelsForLabel_i_logical,threshFrac);
+    
+    c_wsIDsInCell{i} = wsIDsForLabel_i_filtered; 
+    regionIDs = wsIDsForLabel_i_filtered - 1;
     regionIDs = regionIDs(regionIDs>0);
     % get all edges for the cell
     edgesForCell_i = setOfRegions_i(regionIDs,:);
@@ -39,3 +48,17 @@ parfor i=1:numLabels
 end
 
 
+function wsIDs_filtered = getGoodWSregions(wsIDsForLabel_i,ws,pixForLabel_j_logical,threshFrac)
+wsIDs_filtered = [];
+numWsRegions = numel(wsIDsForLabel_i);
+for i=1:numWsRegions
+    wsID_i = wsIDsForLabel_i(i);
+    wsPixels_i_logical = (ws==wsID_i);
+    wsPixOutsideLabelj_logical = ((pixForLabel_j_logical - wsPixels_i_logical)==-1);
+    numWsPixOutside = sum(sum(wsPixOutsideLabelj_logical));
+    numWsPixTot = sum(sum(wsPixels_i_logical));
+    fractionOutside = numWsPixOutside/numWsPixTot;
+    if (fractionOutside<threshFrac)
+        wsIDs_filtered = [wsIDs_filtered; wsID_i];
+    end
+end
