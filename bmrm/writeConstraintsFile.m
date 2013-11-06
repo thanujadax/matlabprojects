398663afd678bb514d95dd629b1ac3988e2fb2f3
thanujadax <thanujadax@gmail.com>
2013-11-07 00:41:48 +0100

    constraint.txt implementation

--- bmrm/writeConstraintsFile.m
+++ bmrm/writeConstraintsFile.m
@@ -1,4 +1,4 @@
-function constraints = writeConstraintsFile(Aeq,senseArray)
+function constraints = writeConstraintsFile(Aeq,b,senseArray)
 
 % create constraints.txt
 % # contains one linear constraints per row on the allowed labels in the following form:
@@ -15,17 +15,45 @@ function constraints = writeConstraintsFile(Aeq,senseArray)
 
 [numConstraints,numVar] = size(Aeq);
 
+% var_num starts from zero
+
 for i=1:numConstraints
     rel1 = senseArray(i);
     switch rel1
         case '='
-            rel = '==';
+            rel = ' == ';
         case '>'
-            rel = '>=';
+            rel = ' >= ';
         case '<'
-            rel = '<=';
+            rel = ' <= ';
     end
     
+    [r,c,coeff] = find(Aeq(i,:));
+    var_num = c - 1; % var_num starts with zero
+    
+    term = '%4.4f*%d ';
+    numTerms = numel(coeff);
+    bstr = int2str(b(i));
+%     formatSpec = repmat(term,1,numTerms);
+%     
+%     
+%     formatSpec = strcat(formatSpec,rel);
+%    
+%     formatSpec = strcat(formatSpec,bstr);
+%     
+%     sprintf(formatSpec,coeff',var_num')
+    str = [];
+    for j=1:numTerms
+       str1 = sprintf(term,coeff(j),var_num(j));
+       %str1 = horzcat(' ',str1);
+       str = horzcat(str,str1);
+    end
+    
+    str = horzcat(str,rel);
+    str = horzcat(str,bstr);
+    str
     
     
 end
+
+constraints = 0;
