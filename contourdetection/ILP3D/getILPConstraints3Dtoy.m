a56f9c6905f32caaa490570811718847c4d1f6ed
thanujadax <thanujadax@gmail.com>
2014-02-18 00:19:28 +0100

    i

--- contourdetection/ILP3D/getILPConstraints3Dtoy.m
+++ contourdetection/ILP3D/getILPConstraints3Dtoy.m
@@ -1,4 +1,4 @@
-function [A,b,senseArray] = getILPConstraints3Dtoy()
+function [A,b,senseArray] = getILPConstraints3Dtoy(c_edgeIDs)
 
 % 2014.02.17
 
@@ -10,10 +10,16 @@ function [A,b,senseArray] = getILPConstraints3Dtoy()
 % Inputs
 
 %% Init
+rowStop = 0;
+numSections = size(c_edgeIDs,2);
 
+totRowsA = 
+totColsA = 
+A = sparse(totRowsA,totColsA);
 %% 2D constraints
 % formulated for each of the sections
 
 %% 3D constraints
 % formulated for each adjacent pair of sections
+
 
