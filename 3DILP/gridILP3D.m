e909a501a32e68e112b6675ed2fb28f8498ead6c
thanujadax <thanujadax@gmail.com>
2014-02-25 18:27:41 +0100

    i

--- 3DILP/gridILP3D.m
+++ 3DILP/gridILP3D.m
@@ -4,11 +4,43 @@ function seg3D = gridILP3D
 % 2014.02.25
 
 %% Parameters
+verbose = 2; % 0,1,2
+pathForInputImages = '';
+fileNameString = '*.tif';
+
 
 %% Read inputs
-
+inputSections = dir(strcat(pathForImages_training,fileNameString)); 
 %% Create 3D grid
 
-%% ILP
+%% Unary activation scores from RFCs
 
+%% ILP formulation
+
+% constraints
+[model.A,b,senseArray] = getILP3DGridConstraints(cellStats);
+% objective to minimize
+f = getILP3DGridObjective(W,cellUnaries);
+
+%% ILP solver
+disp('using Gurobi ILP solver...');
+% model.A = sparse(double(A));
+model.rhs = b;
+model.obj = f';
+model.sense = senseArray;
+% model.vtype = vtypeArray;
+model.vtype = 'B';
+% model.lb = lbArray;
+% model.ub = ubArray;
+model.modelname = '3D_Grid_ILP';
+% initial guess
+% model.start = labelVector;
+
+params.LogFile = 'gurobi_3D_Grid_ILP.log';
+params.Presolve = 0;
+params.ResultFile = 'modelfile_3D_Grid_ILP.mps';
+params.InfUnbdInfo = 1;
+
+resultGurobi = gurobi(model,params);
+x = resultGurobi.x;
 %% Visualization
