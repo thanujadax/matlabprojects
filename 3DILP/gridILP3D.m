a63b76504c70e9b3f890d6885f53c96474f7da80
thanujadax <thanujadax@gmail.com>
2014-02-26 12:05:06 +0100

    i

--- 3DILP/gridILP3D.m
+++ 3DILP/gridILP3D.m
@@ -1,26 +1,77 @@
-function seg3D = gridILP3D
+function seg3D = gridILP3D()
 % 3D ILP on 3D grid
 % Version 1.0
 % 2014.02.25
 
 %% Parameters
 verbose = 2; % 0,1,2
-pathForInputImages = '';
-fileNameString = '*.tif';
+pathForInputImages = '/home/thanuja/Dropbox/data/3D_Grid_ILP/stack1/';
+fileNameString = '*.png';
+gridResX = 4; % num pix
+gridResY = 4;
+gridResZ = 6; % distance between 2 adjacent slices in pixels
 
+NUM_VAR_PER_CELL = 7; 
+% 1 - cell internal state
+% 2 - face xy front
+% 3 - face xy back
+% 4 - face yz left
+% 5 - face yz right
+% 6 - face xz top
+% 7 - face xz bottom
 
 %% Read inputs
-inputSections = dir(strcat(pathForImages_training,fileNameString)); 
-%% Create 3D grid
+inputSections = dir(strcat(pathForInputImages,fileNameString));
+numInputSections = length(inputSections);
+% load images to 3D matrix
+% read the first file to read the dimensions
+inputImage1_FilePath = fullfile(pathForInputImages,inputSections(1).name);
+im1 = uint8(imread(inputImage1_FilePath)); % uint8,single,double are the available options
+[numR,numC] = size(im1);
+clear im1
+% Adjusting image dimensions to include boundary (membrane) cells
+numR = numR + gridResY*2;
+numC = numC + gridResX*2;
+numZ = numInputSections + 2; % Thickness of slices not yet considered
+inputImageStackMat = uint8(ones(numR,numC,numZ));
+rStart = gridResY + 1;
+rStop = numR - gridResY;
+cStart = gridResX + 1;
+cStop = numC - gridResX;
+for i=2:numInputSections
+    % section 1 is already filled with 1s (membrane)
+    inputImageFilePath = fullfile(pathForInputImages,inputSections(i).name);
+    inputImageStackMat(rStart:rStop,cStart:cStop,i) = ...
+                uint8(imread(inputImageFilePath)); 
+    % uint8,single,double are the available options
+end
 
+%% Create 3D grid
+% addressing mechanism: gridID <--> pixels(of particular section)
+% each gridCell correspond to a cube.
+% each cube is made by expanding the corresponding anisotropic image patch
+% vector: gridID|sectionID|pixels
+
+% Membrane cubes are added to the boundary
+griIDs_sectionIDs_pixIDs = getGridCells(inputImageStackMat);
+numCells = size(griIDs_sectionIDs_pixIDs,1);
 %% Unary activation scores from RFCs
+unaryScoreMat = zeros(numCells,NUM_VAR_PER_CELL);
+
+unaryScoreMat(:,1) = getGridCellProbs(); % gridCellProbs_interior
+unaryScoreMat(:,2) = getGridCellFaceProbs_xy(); % gridCellFaceProbs_xy_1 
+unaryScoreMat(:,3) = getGridCellFaceProbs_xy(); % gridCellFaceProbs_xy_2 
+unaryScoreMat(:,4) = getGridCellFaceProbs_sides(); % gridCellFaceProbs_yz_3 
+unaryScoreMat(:,5) = getGridCellFaceProbs_sides(); % gridCellFaceProbs_yz_4 
+unaryScoreMat(:,6) = getGridCellFaceProbs_sides(); % gridCellFaceProbs_xz_5 
+unaryScoreMat(:,7) = getGridCellFaceProbs_sides(); % gridCellFaceProbs_xz_6 
 
 %% ILP formulation
 
 % constraints
 [model.A,b,senseArray] = getILP3DGridConstraints(cellStats);
 % objective to minimize
-f = getILP3DGridObjective(W,cellUnaries);
+f = getILP3DGridObjective(W,unaryScoreMat);
 
 %% ILP solver
 disp('using Gurobi ILP solver...');
@@ -44,3 +95,4 @@ params.InfUnbdInfo = 1;
 resultGurobi = gurobi(model,params);
 x = resultGurobi.x;
 %% Visualization
+seg3D = [];
--- common/readwriteimages.m
+++ common/readwriteimages.m
@@ -1,19 +1,19 @@
 % Read png file
 % filename = '/home/thanuja/matlabprojects/data/mitoData/labels/stem1_membranes.png';
 % filename = '/home/thanuja/matlabprojects/data/mitoData/stem.tiff';
-% filename = '/home/thanuja/Dropbox/data/em_2013january/raw/00.tif';
+filename = '/home/thanuja/Dropbox/data/em_2013january/raw/02.tif';
 % filename = '/home/thanuja/Dropbox/data/RF_training_mem/I00_trainingLabels.tif';
-filename = '/home/thanuja/Dropbox/data/em_2013january/neurons/05.tiff';
+% filename = '/home/thanuja/Dropbox/data/em_2013january/neurons/01.tiff';
 fmt = 'tif';
 A = imread(filename, fmt);
 % C = rgb2gray(A);
 % write to another file
 % writefile = '/home/thanuja/Dropbox/data/em_2013january/samples/raw00_512.png';
 % writefile = '/home/thanuja/Dropbox/data/RF_training_edge/I01_trainingLabels.tif';
-writeFilePath = '/home/thanuja/Dropbox/data/evaldata/input';
+writeFilePath = '/home/thanuja/Dropbox/data/3D_Grid_ILP/stack1/';
 
-dimx = 256;
-dimy = 256;
+dimx = 128;
+dimy = 128;
 
 startRow = 1;
 stopRow = dimy;
@@ -22,28 +22,29 @@ startCol = 1;
 stopCol = dimx;
 
 numDim = 3;
+writeFileName = '02.png';
+writeFileName = strcat(writeFilePath,writeFileName);
+B = A(startRow:stopRow,startCol:stopCol,:);
+imwrite(B,writeFileName,'png')
+figure;imshow(B);
 
-% B = A(startRow:stopRow,startCol:stopCol,:);
-% imwrite(B,writefile,'tif')
-% figure;imshow(B);
-
-k = 00; % file index
-for i=1:4
-    for j=1:4
-        B = A(startRow:stopRow,startCol:stopCol,:);
-        writeName = sprintf('I%02d_trainingLabels.tif',k);
-        writeFileName = strcat(writeFilePath,writeName);
-        disp(writeFileName)
-        imwrite(B,writeFileName,'tif')
-        % figure;imshow(B);
-        
-        startCol = stopCol + 1;
-        stopCol = stopCol + dimx;
-        k = k + 1;
-    end
-    startRow = stopRow + 1;
-    stopRow = stopRow + dimy;
-    startCol = 1;
-    stopCol = dimx;
-end
+% k = 00; % file index
+% for i=1:4
+%     for j=1:4
+%         B = A(startRow:stopRow,startCol:stopCol,:);
+%         writeName = sprintf('I%02d_trainingLabels.tif',k);
+%         writeFileName = strcat(writeFilePath,writeName);
+%         disp(writeFileName)
+%         imwrite(B,writeFileName,'tif')
+%         % figure;imshow(B);
+%         
+%         startCol = stopCol + 1;
+%         stopCol = stopCol + dimx;
+%         k = k + 1;
+%     end
+%     startRow = stopRow + 1;
+%     stopRow = stopRow + dimy;
+%     startCol = 1;
+%     stopCol = dimx;
+% end
         
