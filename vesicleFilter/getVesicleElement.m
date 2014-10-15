b2aea12e8f5d2239c7f7899e9639964fd8dd2534
thanujadax <thanujadax@gmail.com>
2014-10-15 22:10:07 +0200

    first implementation

--- vesicleFilter/getVesicleElement.m
+++ vesicleFilter/getVesicleElement.m
@@ -1,3 +1,8 @@
 function vesicleElement3D = getVesicleElement(innerRadius,outerRadius)
 % returns the vesicle element to be used in the 3D convolution to detect
 % vesicle like structures
+
+vesicleElement3D = zeros(outerRadius*2,outerRadius*2,outerRadius*2);
+[x y z] = meshgrid(1:outerRadius*2,1:outerRadius*2,1:outerRadius*2);
+
+% create outerSphere
--- vesicleFilter/main_vesicleFilter.m
+++ vesicleFilter/main_vesicleFilter.m
@@ -1,8 +1,16 @@
 %% read tiff stack into 3D matrix
 % tif file
 inputImageVolumeFileName = '/home/thanuja/Dropbox/PROJECTS/MSB/Data/raw/GAS1_mss1_small_50.tif';
+outputImageVolumeFileName = '/home/thanuja/Dropbox/PROJECTS/vesicleFilter/small50_1.tif';
+
 imageInfo = iminfo(inputImageVolumeFileName);
 %% convolution with 3D structure element (vesicle)
+vesicleElement3D = getVesicleElement(innerRadius,outerRadius);
 
+% convn (or fftn for fft based convolution)
+
+% interpn: to interpolate the convolution result at the points you want
 
 %% visualize
+imwrite(convolutionOutput,outputImageVolumeFileName,'tif');
+% currently to visualize the tiff volume, we are using fiji
