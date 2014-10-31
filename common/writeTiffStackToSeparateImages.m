2a3ad68b28f94927e0a466c2123f251f6cea58c3
thanujadax <thanujadax@gmail.com>
2014-10-31 04:34:53 +0100

    i

--- common/writeTiffStackToSeparateImages.m
+++ common/writeTiffStackToSeparateImages.m
@@ -1,11 +1,16 @@
-function writeTiffStackToSeparateImages(inputImageVolumeFileName)
+function writeTiffStackToSeparateImages(inputImageVolumeFileName,saveFilePath,fileType)
 
 inputImageStack = readTiffStackToArray(inputImageVolumeFileName);
 
 numImg = size(inputImageStack,3);
 
-fileType = 'tif';
+%fileType = 'tif';
 
 for i=1:numImg
     imageName = sprintf('%d.%s',(i-1),fileType);
+    outputFullFile = fullfile(saveFilePath,imageName);
+    disp(outputFullFile);
+    image_i = inputImageStack(:,:,i);
+    imagesc(image_i);
+    imwrite(image_i,outputFullFile);
 end
