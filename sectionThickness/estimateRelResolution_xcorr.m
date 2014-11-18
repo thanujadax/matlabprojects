03c2efce48d65526d463bfd7250cbf033199e483
Thanuja Ambegoda <thanuja@ini.ch>
2014-11-18 11:37:11 +0100

    added method to predict section thickness using xcorr

---
+++ sectionThickness/estimateRelResolution_xcorr.m
@@ -0,0 +1,22 @@
+function [g,m12] = estimateRelResolution_xcorr(I,maxG)
+% based on autocorrelation
+
+% I - 2 images packed as NxMx2
+% maxG - maximum sampling distance to evaluate
+
+normalize = @(I) (I-mean(I(:)))/std(I(:));
+I(:,:,1) = normalize(I(:,:,1));
+I(:,:,2) = normalize(I(:,:,2));
+m12 = zeros(1,maxG);
+
+for g = 1:maxG
+    m12(g) = corr2(I(1+g:size(I,1),:,:),I(1:size(I,1)-g,:,:));
+    % d2I = (I(:,1+g:size(I,2),:)-I(:,1:size(I,2)-g,:));
+    % m12(g) = std([d1I(:);d2I(:)]);
+    % autocorr_xy = corr2(d1I,d2I);
+end
+
+% d3I = I(:,:,2)-I(:,:,1);
+% m3 = std(d3I(:));
+m3 = corr2(I(:,:,1),I(:,:,2));
+g = interp1(m12,1:maxG,m3);
