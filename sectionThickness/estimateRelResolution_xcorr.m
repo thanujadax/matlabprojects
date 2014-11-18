da731916d8b95ce22cc94a972c6c63341cf64a41
Thanuja Ambegoda <thanuja@ini.ch>
2014-11-18 13:42:23 +0100

    script for running xcorr based thickness estimation

--- sectionThickness/estimateRelResolution_xcorr.m
+++ sectionThickness/estimateRelResolution_xcorr.m
@@ -1,5 +1,5 @@
 function [g,m12] = estimateRelResolution_xcorr(I,maxG)
-% based on autocorrelation
+% based on correlation coefficient
 
 % I - 2 images packed as NxMx2
 % maxG - maximum sampling distance to evaluate
@@ -9,8 +9,19 @@ I(:,:,1) = normalize(I(:,:,1));
 I(:,:,2) = normalize(I(:,:,2));
 m12 = zeros(1,maxG);
 
+[numR,numC,~] = size(I);
+
 for g = 1:maxG
-    m12(g) = corr2(I(1+g:size(I,1),:,:),I(1:size(I,1)-g,:,:));
+    
+    A = zeros(numR-g,numC);
+    B = zeros(numR-g,numC);
+    
+    A(:,:) = I(1+g:size(I,1),:,2);
+    B(:,:) = I(1:size(I,1)-g,:,2);
+    
+    m12(g) = corr2(A,B);
+    
+    
     % d2I = (I(:,1+g:size(I,2),:)-I(:,1:size(I,2)-g,:));
     % m12(g) = std([d1I(:);d2I(:)]);
     % autocorr_xy = corr2(d1I,d2I);
--- sectionThickness/script_estimateRelResolution.m
+++ sectionThickness/script_estimateRelResolution.m
@@ -1,13 +1,13 @@
 % script to run thickness estimation
 
-% inputImageFileName = '/home/thanuja/Dropbox/data/fibsem/smallSet2/raw/2.png';
-inputImageFileName = '/home/thanuja/projects/inputData/trainingHalf/raw/00.png';
+inputImageFileName = '/home/thanuja/Dropbox/data/fibsem/smallSet2/raw/2.png';
+% inputImageFileName = '/home/thanuja/projects/inputData/trainingHalf/raw/00.png';
 
 inputImageMat1 = double(imread(inputImageFileName));
 inputImageMat1 = inputImageMat1./255;
 
-% inputImageFileName = '/home/thanuja/Dropbox/data/fibsem/smallSet2/raw/3.png';
-inputImageFileName = '/home/thanuja/projects/inputData/trainingHalf/raw/01.png';
+inputImageFileName = '/home/thanuja/Dropbox/data/fibsem/smallSet2/raw/3.png';
+% inputImageFileName = '/home/thanuja/projects/inputData/trainingHalf/raw/01.png';
 
 inputImageMat2 = double(imread(inputImageFileName));
 inputImageMat2 = inputImageMat2./255;
@@ -20,4 +20,4 @@ Imat(:,:,2) = inputImageMat2;
 
 maxG = 15; % maximum sampling distance to evaluate
 
-[g,m12] = estimateSamplingRatio(Imat,maxG);
+[g,m12] = estimateRelResolution_xcorr(Imat,maxG);
