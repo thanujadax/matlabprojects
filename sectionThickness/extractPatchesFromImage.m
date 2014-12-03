8ea8d39597a3ec3d8c0f252ac0944a326487880d
Thanuja Ambegoda <thanuja@ini.ch>
2014-12-04 00:45:47 +0100

    bug fix. todo: getCorrespondingPatch

--- sectionThickness/extractPatchesFromImage.m
+++ sectionThickness/extractPatchesFromImage.m
@@ -14,16 +14,24 @@ else
     rStartVect = 1:patchSizeR:sizeR;
     cStartVect = 1:patchSizeC:sizeC;
     
+    rStartVect(end) = [];
+    cStartVect(end) = [];
+    
     numPatches_1 = numel(rStartVect) * numel(cStartVect);
     imagePatches = zeros(patchSizeR,patchSizeC,numPatches_1);
     
-    for i=1:numPatches_1
+    k = 1;
+    
+    for i=1:numel(rStartVect)
         rStart = rStartVect(i);
         rStop = rStart + patchSizeR -1;
-        cStart = cStartVect(i);
-        cStop = cStart + patchSizeC -1;
-        imagePatches(:,:,i) = inputImage...
+        for j=1:numel(cStartVect)
+            cStart = cStartVect(j);
+            cStop = cStart + patchSizeC -1;
+            imagePatches(:,:,k) = inputImage...
             (rStart:rStop,cStart:cStop);
+            k = k+1;
+        end
     end
 % if numPatches > maxNumPatches, return a random subset
 
--- sectionThickness/getRelativeDistance_cc.m
+++ sectionThickness/getRelativeDistance_cc.m
@@ -1,6 +1,8 @@
 function relativeDistancePix = getRelativeDistance_cc(imageName1,imageName2,curve,maxVal)
 % predicts relative section interval in terms of the pixel length in xy
 % plane, using correlation coefficient
+% Inputs:
+% image names, curve, maxval
 
 I1 = double(imread(imageName1));
 I2 = double(imread(imageName2));
