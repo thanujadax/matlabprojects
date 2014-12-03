05366808d53e0904bac4a0cf21c81c181bafa648
thanujadax <thanujadax@gmail.com>
2014-12-03 19:07:01 +0100

    getThicknessCuve implemented

--- sectionThickness/getThicknessCurve.m
+++ sectionThickness/getThicknessCurve.m
@@ -1,8 +1,27 @@
-function [thicknessCurve, t_var] = getThicknessCurve(imageFileDir)
+function [thicknessCurve, t_std] = getThicknessCurve...
+                                (inputImageDir,imageType,maxShift)
 
 % Inputs:
-%   imageFileDir - input image file directory
+%   inputImageDir
+%   maxShift - max shift in pixels to consider for the curve
 
 % Outputs:
 %   thicknessCurve - correlation coefficient vs pix gap  
 
+
+allImageFiles = dir(fullfile(inputImageDir,strcat('*.',imageType)));
+numImg = length(allImageFiles);
+
+xcorrMat = zeros(numImg,maxShift);
+
+for i=1:numImg
+    imageFileName = fullfile(inputImageDir,allImageFiles(i).name);
+    if(xcorrMeasure)
+        xcorrMat(i,:) = getXcorrShiftedImg(imageFileName,maxShift);
+    else
+        xcorrMat(i,:) = getIntensityDeviationShiftedImg(imageFileName,maxShift);
+    end
+end
+
+thicknessCurve = mean(xcorrMat,2);
+t_std = std(xcorrMat,0,2);
