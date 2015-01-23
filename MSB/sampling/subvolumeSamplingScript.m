f5c4afaa3f46cb84c202e4f3486ad86ba0602e34
thanujadax <thanujadax@gmail.com>
2015-01-23 18:02:30 +0100

    started subvol random sampling script

--- MSB/sampling/subvolumeSamplingScript.m
+++ MSB/sampling/subvolumeSamplingScript.m
@@ -12,7 +12,7 @@ spreadSheetFileName = '/home/thanuja/Dropbox/PROJECTS/MSB/annotations/xls/s108_m
 % x,y,z of each msb
 [num,txt,raw] = xlsread(spreadSheetFileName);
 
-% define boundaries of subvolumes
+%% define boundaries of subvolumes
 % z
 v1234_z_start = 1;
 v1234_z_stop = 400;
@@ -44,6 +44,23 @@ v56_y_stop = 800;
 v78_y_start = 801;
 v78_y_stop = 1600;
 
-
-
-% sample subvolumes
+%% sample subvolumes
+for i=2:size(num,1)
+    z = num(i,2);
+    x = num(i,3);
+    y = num(i,4);
+    
+    if(z<v5678_z_start)
+        % should be 1 or 2 or 3 or 4
+        if(x<v24_x_start)
+            % should be 1 or 3
+            
+        else
+            % should be 2 or 4
+        end
+    else
+        % should be 5 or 6 or 7 or 8
+        
+    end
+    
+end
