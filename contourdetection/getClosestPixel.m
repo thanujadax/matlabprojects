1625d925a91287d3c0fb1bff14d3387fefd9579c
thanujadax <thanujadax@gmail.com>
2015-02-02 19:39:37 +0100

    bug fix

--- contourdetection/getClosestPixel.m
+++ contourdetection/getClosestPixel.m
@@ -2,7 +2,7 @@ function closestPixelInd = getClosestPixel(refPixInd,pixIndList,sizeR,sizeC)
 
 [refR,refC] = ind2sub([sizeR sizeC],refPixInd);
 
-[pixListR, pixeListC] = ind2sub([sizeR sizeC],pixIndList);
+[pixListR, pixListC] = ind2sub([sizeR sizeC],pixIndList);
 
 distance = (pixListR - refR).^2 + (pixListC - refC).^2;
 
