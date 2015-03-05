65c36e576a5fc95cbe03cc528e2815665e667f3e
thanujadax <thanujadax@gmail.com>
2015-03-05 14:18:27 +0100

    fixed bugs in subvolume sampling script

--- MSB/sampling/subvolumeSamplingScript.m
+++ MSB/sampling/subvolumeSamplingScript.m
@@ -21,6 +21,10 @@ disp('Reading xls file')
 spreadSheetFileName = '/home/thanuja/Dropbox/PROJECTS/MSB/annotations/xls/s108_msb_mss.xls';
 disp(spreadSheetFileName)
 
+readKeyPointsFromFile = 1;
+%sampleID = 108;
+keyPointsFileName = '/home/thanuja/Dropbox/PROJECTS/MSB/annotations/subVolumeKeyPoints.xls';
+
 % read spreadsheet
 % x,y,z of each msb
 [num,txt,raw] = xlsread(spreadSheetFileName);
@@ -31,26 +35,32 @@ cells_msbSubVolumes{8} = [];
 %% define key points of subvolumes (in pixel coordinates)
 % NEED TO BE DEFINED FOR EACH VOLUME
 % (local origins: 1,2,3 of the volume in terms of global pixel coordinates)
+if(readKeyPointsFromFile)
+    [keyPointsList,txt2,raw2] = xlsread(keyPointsFileName);
+    sampleID = extractSampleIdFromFileName(spreadSheetFileName);
+    [x1,x2,x3,y1,y2,y3,z1,z2,z3,xRange,yRange] = extractKeyPointsForVolume...
+                (keyPointsList,sampleID);
+else
+    % z
+    z1 = 1;
+    z2 = 401;
+    z3 = 800;
 
-% z
-z1 = 1;
-z2 = 401;
-z3 = 800;
+    % x
+    x1 = 1;
+    x2 = 1;
+    x3 = 1;
 
-% x
-x1 = 1;
-x2 = 1;
-x3 = 1;
-
-% y
-y1 = 1;
-y2 = 1;
-y3 = 1;
+    % y
+    y1 = 1;
+    y2 = 1;
+    y3 = 1;
 
 % general image width 
 xRange = 1600;
 yRange = 1600;
 
+end
 %% sample subvolumes
 disp('Iterating through the list of points ..')
 for i=2:size(num,1)
@@ -64,7 +74,9 @@ for i=2:size(num,1)
         xL = (x1+x2)/2;
         xR = xL + xRange;
         xM = (xL+xR)/2; % TODO replace with weighted avg depending on current section
-        yM = ((y1+y2)/2 + yRange)/2;
+        yU = (y1+y2)/2;
+        yD = yU + yRange;
+        yM = (yU + yD)/2;
         
         if(x<xM)
             % should be 1 or 3
@@ -91,7 +103,9 @@ for i=2:size(num,1)
         xL = (x3+x2)/2;
         xR = xL + xRange;
         xM = (xL+xR)/2; % TODO replace with weighted avg depending on current section
-        yM = ((y3+y2)/2 + yRange)/2;        
+        yU = (y3+y2)/2;
+        yD = yU + yRange;
+        yM = (yU + yD)/2;       
 
         if(x<xM)
             % should be 5 or 7
@@ -116,3 +130,33 @@ for i=2:size(num,1)
     
 end
 disp('.. finished iterating the list')
+
+function sampleID = extractSampleIdFromFileName(spreadSheetFileName)
+tokenizedPaths = strsplit(spreadSheetFileName,'/');
+fileName = tokenizedPaths{end};
+c_sampleName = strsplit(fileName,'_');
+c_sampleID = strsplit(c_sampleName{1},'s');
+sampleID = c_sampleID{2};
+
+function [x1,x2,x3,y1,y2,y3,z1,z2,z3,xRange,yRange] = extractKeyPointsForVolume...
+                (keyPointsList,sampleID)
+
+rowID = find(keyPointsList(:,1)==str2num(sampleID));
+if(isempty(rowID))
+    error('SampleID not found in key points file. Please check!')
+else
+    x1 = keyPointsList(rowID,2);
+    y1 = keyPointsList(rowID,3);
+    z1 = keyPointsList(rowID,4);
+    x2 = keyPointsList(rowID,5);
+    y2 = keyPointsList(rowID,6);
+    z2 = keyPointsList(rowID,7);
+    x3 = keyPointsList(rowID,8);
+    y3 = keyPointsList(rowID,9);
+    z3 = keyPointsList(rowID,10);    
+    
+    xRange = keyPointsList(rowID,11);
+    yRange = keyPointsList(rowID,12);
+
+end
+            
