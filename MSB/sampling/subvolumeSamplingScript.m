d29da4405123e7dce75660b8d26a83f8662a26a6
thanujadax <thanujadax@gmail.com>
2015-03-05 11:10:23 +0100

    subvolume sampling function done

--- MSB/sampling/subvolumeSamplingScript.m
+++ MSB/sampling/subvolumeSamplingScript.m
@@ -1,25 +1,36 @@
+function cells_msbSubVolumes = subvolumeSamplingScript()
+
 % sample subvolumes
+% key points of each volume have to be defined before using the script
 
 % Inputs:
 %   spreadsheet - containing msbs
 
 % Outputs
-%   numbers of msbs of 4 equal subvolumes
+%   msbs (IDs) of 4 equal subvolumes
 
 % _ _        _ _
 %|1|2|      |5|6|
 % - -   ,    - -
 %|3|4|      |7|8|
 % - -        - -
+disp('Sub volume analysis..')
+disp('CAUTION: make sure local origins are defined properly for this volume!')
 
+disp('Reading xls file')
 spreadSheetFileName = '/home/thanuja/Dropbox/PROJECTS/MSB/annotations/xls/s108_msb_mss.xls';
+disp(spreadSheetFileName)
 
 % read spreadsheet
 % x,y,z of each msb
 [num,txt,raw] = xlsread(spreadSheetFileName);
-
+str1 = sprintf('loaded %d points from the xls file',size(num,1));
+disp(str1)
+% Output data structures
+cells_msbSubVolumes{8} = [];
 %% define key points of subvolumes (in pixel coordinates)
-% (local origins)
+% NEED TO BE DEFINED FOR EACH VOLUME
+% (local origins: 1,2,3 of the volume in terms of global pixel coordinates)
 
 % z
 z1 = 1;
@@ -41,6 +52,7 @@ xRange = 1600;
 yRange = 1600;
 
 %% sample subvolumes
+disp('Iterating through the list of points ..')
 for i=2:size(num,1)
     % iterate through the list of coordinates given in the xls file
     z = num(i,2);
@@ -56,14 +68,51 @@ for i=2:size(num,1)
         
         if(x<xM)
             % should be 1 or 3
-             
-            
+            if(y<yM)
+                % 1
+                %subVol1 = [subVol1; (i-1)];
+                cells_msbSubVolumes{1}(end+1) = (i-1); 
+            else
+                % 3
+                cells_msbSubVolumes{3}(end+1) = (i-1);
+            end
         else
             % should be 2 or 4
+            if(y<yM)
+                % 2
+                cells_msbSubVolumes{2}(end+1) = (i-1);
+            else
+                % 4
+                cells_msbSubVolumes{4}(end+1) = (i-1);
+            end
         end
     else
         % should be 5 or 6 or 7 or 8
-        
+        xL = (x3+x2)/2;
+        xR = xL + xRange;
+        xM = (xL+xR)/2; % TODO replace with weighted avg depending on current section
+        yM = ((y3+y2)/2 + yRange)/2;        
+
+        if(x<xM)
+            % should be 5 or 7
+            if(y<yM)
+                % 5
+                cells_msbSubVolumes{5}(end+1) = (i-1);
+            else
+                % 7
+                cells_msbSubVolumes{7}(end+1) = (i-1);
+            end
+        else
+            % should be 6 or 8
+            if(y<yM)
+                % 6
+                cells_msbSubVolumes{6}(end+1) = (i-1);
+            else
+                % 8
+                cells_msbSubVolumes{8}(end+1) = (i-1);
+            end
+        end        
     end
     
 end
+disp('.. finished iterating the list')
