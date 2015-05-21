810a93c063feddf5e36dc183f72d4b45d145052d
Thanuja Ambegoda <thanujadax@gmail.com>
2015-05-21 16:40:37 +0200

    calculation of rescaling factor x,y ->z

--- sectionThickness/makeEnsembleDecayCurveForVolume.m
+++ sectionThickness/makeEnsembleDecayCurveForVolume.m
@@ -1,11 +1,39 @@
-function makeEnsembleDecayCurveForVolume()
+function [y,errBar] = makeEnsembleDecayCurveForVolume(matFilePath,fileStr,zDirection)
 
-matFilePath = '/home/thanuja/projects/tests/thickness/similarityCurves/20150512/s108';
-fileStr = 'xcorrMat'; % general string that defines the .mat file
+% Inputs:
+% matFilePath = '/home/thanuja/projects/tests/thickness/similarityCurves/20150512/s108';
+% fileStr = 'xcorrMat'; % general string that defines the .mat file
+% zDirection = 1; % 0 for x,y direction decay curves
+
+if(zDirection)
+    directionStr = 'z direction';
+    plotColor = 'b';
+else
+    directionStr = 'x,y directions';
+    plotColor = 'g';
+end
 
 % read relevant mat files
-
 
 % combine them
 % get mean and variance
 % plot smoot curve - using interpolation ?
+
+combinedMat = readAllMatFiles(matFilePath,fileStr,zDirection);
+[numR,numC] = size(combinedMat);
+% each row corresponds to a different image. each column correspond to c.c.
+% at different distances
+
+y = mean(combinedMat,1);
+errBar = std(combinedMat);
+
+%% Plot
+tokenizedSubDirName = strsplit(matFilePath,filesep);
+volName = tokenizedSubDirName{end};
+titleStr = sprintf('Ensemble similarity decay curve: vol %s - %s',volName,directionStr);
+xlabelStr = 'Distance (#px or #sections)';
+ylabelStr = 'Coefficient of Correlation';
+transparent = 0;
+figure;
+shadedErrorBar((0:(numC-1)),y,errBar,plotColor,transparent,...
+    titleStr,xlabelStr,ylabelStr);
--- sectionThickness/readAllMatFiles.m
+++ sectionThickness/readAllMatFiles.m
@@ -6,7 +6,7 @@ function combinedMat = readAllMatFiles(matFilePath,fileStr,z)
 %   each column corresponds to a different distance
 %   so all the mat files can be appended beneath the previous one
 
-matFileNames = '*.mat';
+matFileNames = strcat(fileStr,'*.mat');
 matFileNames = fullfile(matFilePath,matFileNames);
 
 matFileDir = dir(matFileNames);
@@ -14,8 +14,29 @@ TotNumCurves = length(matFileDir);
 
 if(z)
     % along z axis. only 3 curves to be combined
-    numCurves = 3; % 8,9,10
+    indices = [8,9,10];
+    % numCurves = numel(indices); % 8,9,10
+    disp('Reading .mat files ending with 08, 09 and 10 - along z axis')
+    
 else
     % x,y directions. i.e. 6 curves
-    numCurves = 6; % 1,2,4,5,6,7
+    indices = [1,2,4,5,6,7];
+    % numCurves = 6; % 1,2,4,5,6,7
+    disp('Reading .mat files ending with 1,2,4,5,6,7 - along x,y')
+        
+end
+
+combinedMat = readMats(indices,matFilePath,matFileDir);
+
+
+function mat = readMats(indices,matFilePath,matFileDir)
+
+mat = [];
+numInds = length(indices);
+
+for i=1:numInds
+    clear xcorrMat
+    load(fullfile(matFilePath,matFileDir(indices(i)).name));
+    mat = [mat; xcorrMat];
+    
 end
